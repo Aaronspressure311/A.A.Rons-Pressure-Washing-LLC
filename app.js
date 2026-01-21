c0fef50b4c64fa41f2731422853f9dd14ee35f1a
Aaronspressure311 <a.a.ronspressurewashing311@gmail.com>
2026-01-20 20:49:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("A.A. Ron’s Pressure Washing App Loaded");
+function showTab(tabId) {
+  document.querySelectorAll('section').forEach(sec => sec.style.display = 'none');
+  document.getElementById(tabId).style.display = 'block';
+}
+
+// Show Home tab by default
+showTab('home');
